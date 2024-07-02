e5f160fa02421e4cccd2c7feb413a1b8a7197e64
IvanC <respProjetcsDevelop@gmail.com>
2024-07-01 19:00:06 -0600

    Agregar archivos de proyecto.

---
+++ SATIN/View/Acceso/LoginView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SATIN.View
+{
+    /// <summary>
+    /// Lógica de interacción para LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) DragMove();            
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState= WindowState.Minimized;
+        }
+
+        private void btnCerrar_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
